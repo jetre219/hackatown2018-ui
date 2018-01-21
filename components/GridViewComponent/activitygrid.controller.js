ebec3adc717cd52cc26d3ada2eb11658295b7c07
Philippe Giroux-Ayotte <payotte@modulusdata.com>
2018-01-21 02:21:51 -0500

    switch from grid to list

--- components/GridViewComponent/activitygrid.controller.js
+++ components/GridViewComponent/activitygrid.controller.js
@@ -5,6 +5,8 @@ activityGrid.controller('activityGridController',
 
 function ActivityGridController($scope, $rootScope, $api) {
 
+	$rootScope.categorySelected = false;
+
 	$api.venues().then(function success(response){
         $rootScope.venues = addPromotion(response.data.response.venues);
         $scope.categories = [
@@ -38,17 +40,36 @@ function ActivityGridController($scope, $rootScope, $api) {
     function addPromotion(venues){
         venues.forEach(function (venue){
             var randomNumber = Math.floor((Math.random() * 10) + 1);
-            var promotion = Object();
+            var promotion = {};
             promotion.haspromotion = false;
             if(randomNumber < 4){
                 promotion.haspromotion = true;
                 promotion.necessarypeople = Math.floor((Math.random() * 5) + 2);
                 promotion.description = "10% off on 20$ purchase and more!";
-                //promotion.qrcode = createQrCode();
+                promotion.qrcode = createQrCode();
             }
             venue.promotion = promotion;
         });
         return venues;
+    }
+
+    function createQrCode(){
+        var typeNumber = 4;
+        var errorCorrectionLevel = 'L';
+        var qr = qrcode(typeNumber, errorCorrectionLevel);
+        qr.addData(makeqrid());
+        qr.make();
+        return qr;
+	}
+
+	function makeqrid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 5; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
     }
 
     function amountOfVenuesWithPromotion(){
@@ -61,6 +82,9 @@ function ActivityGridController($scope, $rootScope, $api) {
     	return venuesWithPromotion;
     }
 
-	console.log($rootScope.venues);
+	$rootScope.selectCategory = function (){
+		$rootScope.categorySelected = !$rootScope.categorySelected;
+		console.log($rootScope.categorySelected);
+	}
 	
 }
