5cd39a320299da63f5ef249161df43bee7b60bd4
Jeffrey Tremblay <jeffrey.tremblay.1@ulaval.ca>
2018-01-21 07:05:37 -0500

    hbjgfsdbhjà

--- components/ListViewComponent/activitylist.controller.js
+++ components/ListViewComponent/activitylist.controller.js
@@ -4,6 +4,12 @@ activityList.controller('activityListController',
     ['$scope', '$api',  ActivityListController]);
 
 function ActivityListController($scope, $api) {
+    var typeNumber = 4;
+    var errorCorrectionLevel = 'L';
+    var qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData('Hi!');
+    qr.make();
+    console.log("test");
+    $scope.imgElement = qr.createImgTag();
 
-    
 }
--- components/ListViewComponent/activitylist.module.js
+++ components/ListViewComponent/activitylist.module.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var activityList = angular.module('activityList', [
-
-]);
+      'ngSanitize'
+    ]);
 
 activityList.constant('activityListConfig', {
    'path' : '/components/ListViewComponent'
