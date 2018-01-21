e76251abb057368019dd4c02f068efcee0f9fa75
Philippe Giroux-Ayotte <payotte@modulusdata.com>
2018-01-21 04:53:55 -0500

    working with homecontroller

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -5,6 +5,57 @@ app.controller("HomeCtrl", [
     '$scope',
     function ($rootScope, $scope) {
 
+    	$rootScope.selectedGroup = {};
+
+    	$scope.jobGroup = [
+    		{
+    			"name" : "Jeff",
+    			"lon" : 43,
+    			"lat": 45
+    		},
+    		{
+    			"name": "Will",
+    			"lon":43,
+    			"lat":43
+    		}
+    	];
+
+    	$scope.universityGroup = [
+    		{
+    			"name" : "Alex",
+    			"lon" : 43,
+    			"lat": 45
+    		},
+    		{
+    			"name": "Ted",
+    			"lon":43,
+    			"lat":43
+    		},
+    		{
+    			"name" : "Phil",
+    			"lon" : 43,
+    			"lat": 45
+    		},
+    		{
+    			"name": "Sam",
+    			"lon":43,
+    			"lat":43
+    		},
+    		{
+    			"name" : "Tom",
+    			"lon" : 43,
+    			"lat": 45
+    		},
+    		{
+    			"name": "Etienne",
+    			"lon":43,
+    			"lat":43
+    		}
+    	];
+
+    	$scope.selectGroup = function(group){
+    		$rootScope.selectedGroup = group;
+    	}
 
     }
 ]);
