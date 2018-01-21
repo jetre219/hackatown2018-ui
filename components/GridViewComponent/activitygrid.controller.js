d3dd6cad8473c1df3ca5e7f149d79de884daa9b6
Philippe Giroux-Ayotte <payotte@modulusdata.com>
2018-01-20 19:52:00 -0500

    api venues

--- components/GridViewComponent/activitygrid.controller.js
+++ components/GridViewComponent/activitygrid.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 activityGrid.controller('activityGridController',
-    ['$scope', ActivityGridController]);
+    ['$scope', 'venuesProvider', ActivityGridController]);
 
-function ActivityGridController($scope) {
+function ActivityGridController($scope, venuesProvider) {
+
+	var venues = $api.venues().then(function success(response){
+    	return response.data.response.venues;
+  	});
 
 	$scope.categories = [
 		{
